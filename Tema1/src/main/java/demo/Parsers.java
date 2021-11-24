772682c5857fc9bc49df77e20d45f09864b6b297
Cristian <cris4599al@gmail.com>
2021-11-24 16:02:41 +0200

    Tema1 fixed

---
+++ Tema1/src/main/java/demo/Parsers.java
@@ -0,0 +1,48 @@
+package demo;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Parsers {
+
+    public static String FormatXml(List<Object> items) {
+        StringBuilder tasksXml = new StringBuilder();
+        tasksXml.append("<tasks>");
+
+        items.forEach(task ->
+        {
+            tasksXml.append("\n\t<task>");
+            ((Map<String, Object>) task).forEach((key, value) -> {
+                tasksXml.append(String.format("\n\t\t<%s>\n", key));
+                tasksXml.append(String.format("\t\t\t%s", value));
+                tasksXml.append(String.format("\n\t\t</%s>", key));
+            });
+            tasksXml.append("\n\t</task>");
+        });
+        tasksXml.append("\n</tasks>");
+
+        return tasksXml.toString();
+    }
+
+    public static String FormatCsv(List<Object> values) {
+        //headers = ((Map<String,Object>)values.).values().stream().map((key, value) -> key);
+        //values.forEach(item -> ((Map<String,Object>)item).));
+        List<String> headers = new ArrayList<String>();
+        ((Map<String,Object>)values.get(0)).forEach((key, value) -> headers.add(key));
+        StringBuilder tasksCsv = new StringBuilder();
+        headers.forEach(field -> tasksCsv.append(String.format("%s,", field)));
+        tasksCsv.deleteCharAt(tasksCsv.length() - 1);
+        values.forEach(task ->
+        {
+            tasksCsv.append("\n");
+            headers.forEach(field ->
+                    tasksCsv.append(String.format("%s,", ((Map<String, Object>) task).get(field))));
+            tasksCsv.deleteCharAt(tasksCsv.length() - 1);
+        });
+        return tasksCsv.toString();
+
+    }
+}
